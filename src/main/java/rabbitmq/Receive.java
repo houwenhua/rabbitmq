0553cd0a60905cdf7785d09ccea4abaaba290209
houwenhua <1319427512@qq.com>
2018-02-02 11:43:54 +0800

    first commit rabbitmq

---
+++ src/main/java/rabbitmq/Receive.java
@@ -0,0 +1,54 @@
+package rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class Receive {
+
+	public static void main(String[] args) throws IOException, TimeoutException, ShutdownSignalException, ConsumerCancelledException, InterruptedException {
+		//创建工厂
+		ConnectionFactory cf = new ConnectionFactory();
+		//使用默认端口
+		cf.setHost("localhost");
+		//创建连接
+		Connection connection = cf.newConnection();
+		//声明消息通道
+		Channel channel = connection.createChannel();	
+		
+		//交换机名字
+		String exchangeName = "chatEXG";
+		//队列名
+		String queueName = "chatQueue";
+		//routingKey
+		String routingKey = "RountKey1";
+		//声明交换机，第一个参数是交换机名字，第二个是四种交换机的一种，第三是是否持久化，定义为是，就是服务器重启时，队列依旧存在。
+		channel.exchangeDeclare(exchangeName,"direct",true);
+		//queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete,Map<String, Object> arguments)  
+        //第一个参数是队列名，第二个是是否持久化，第三个表示是否只进行当前的tcp连接，第四个表示是否进行自动删除队列，第五个主要用于Headers Exchange进行消息匹配时
+		channel.queueDeclare(queueName, true, false, false, null);
+		//绑定队列和交换机，Queue.BindOk queueBind(String queue, String exchange, String routingKey)
+		//第一个参数是队列名，第二个参数是交换机名，第三个参数routingKey是消息队列和Exchange之间绑定的路由key
+		channel.queueBind(queueName, exchangeName, routingKey);
+		
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		//basicConsume(String queue, boolean autoAck, Consumer callback)
+		//第一个参数是队列名，第二个是否是自动确认，自动发确认消息(Ack消息)给消息队列，消息队列会将这条消息从消息队列里删除，第三个参数就是Consumer对象，用于处理接收到的消息。
+		channel.basicConsume(queueName, true, consumer);
+		while(true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+            System.out.println("接收信息： '" + message + "'");
+            //回复ack包，不回复，队列不删除,上面的basicConsume(QUEUE_NAME, true, consumer);第二个参数已经是是否自动确认
+            //channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
+		}
+
+	}
+
+}
---
+++ src/main/java/rabbitmq/Send.java
@@ -0,0 +1,57 @@
+package rabbitmq;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+
+		//创建工厂
+		ConnectionFactory cf = new ConnectionFactory();
+		//使用默认端口
+		cf.setHost("localhost");
+		//创建连接
+		Connection connection = cf.newConnection();
+		//声明消息通道
+		Channel channel = connection.createChannel();
+		
+		//交换机名字
+		String exchangeName = "chatEXG";
+		//队列名
+		String queueName = "chatQueue";
+		//routingKey
+		String routingKey = "RountKey1";
+		//声明交换机，第一个参数是交换机名字，第二个是四种交换机的一种，第三是是否持久化，定义为是，就是服务器重启时，队列依旧存在。
+		channel.exchangeDeclare(exchangeName,"direct",true);
+		//queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete,Map<String, Object> arguments)  
+        //第一个参数是队列名，第二个是是否持久化，第三个表示是否只进行当前的tcp连接，第四个表示是否进行自动删除队列，第五个主要用于Headers Exchange进行消息匹配时
+		channel.queueDeclare(queueName, true, false, false, null);
+		//绑定队列和交换机，Queue.BindOk queueBind(String queue, String exchange, String routingKey)
+		//第一个参数是队列名，第二个参数是交换机名，第三个参数routingKey是消息队列和Exchange之间绑定的路由key
+		channel.queueBind(queueName, exchangeName, routingKey);
+		
+		String message = "hello world";
+		while(true) {
+			System.out.println("请输入,输入exit退出:");
+			Scanner sc = new Scanner(System.in);
+			message = sc.nextLine();
+			//basicPublish(String exchange, String routingKey,BasicProperties props, byte[] body)
+			//第一个参数交换机名称，第二个是是消息的路由Key，第三个参数props是消息包含的属性信息，第四个是消息体
+			channel.basicPublish(exchangeName, routingKey, null, message.getBytes());
+			//输入exit退出
+			if(message.equals("exit")){
+				break;
+			}
+		}
+		//关闭管道和连接
+		channel.close();
+		connection.close();
+	}
+
+}
